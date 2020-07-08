f48538eded6dd8d3b8796755f17caade0276a225
flippantgriffin <flippantgriffin@github.com>
2020-07-08 12:25:24 -0500

    get midfielders from england task finished and MidfieldersFromEngland and Squad class added

---
+++ src/main/java/POJO/MidfieldersFromEngland.java
@@ -0,0 +1,15 @@
+package POJO;
+
+import java.util.List;
+
+public class MidfieldersFromEngland {
+    private List<Squad> squad;
+
+    public List<Squad> getSquad() {
+        return squad;
+    }
+
+    public void setSquad(List<Squad> squad) {
+        this.squad = squad;
+    }
+}
